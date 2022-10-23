d9257abbbc4a635f118624cd66a6d19e2806cb61
Heroicacid <105976483+Heroicacid@users.noreply.github.com>
2022-10-23 18:10:24 +0330

    Add files via upload
    for filter-less

---
+++ helpers.c
@@ -0,0 +1,128 @@
+#include "helpers.h"
+#include<math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float avg = (image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3.00;
+            int average = round(avg);
+            
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sepiaGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
+            int sepiaBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
+            int sepiaRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
+            
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtRed = sepiaRed;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (width / 2); j++)
+        {
+            int r = image[i][j].rgbtRed;
+            int b = image[i][j].rgbtBlue;
+            int g = image[i][j].rgbtGreen;
+            
+            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+            
+            image[i][width - j - 1].rgbtGreen = g;
+            image[i][width - j - 1].rgbtBlue = b;
+            image[i][width - j - 1].rgbtRed = r;
+        }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE copy[height][width];
+    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float sumBlue = 0;
+            float sumRed = 0;
+            float sumGreen = 0;
+            int counter = 0;
+            
+            for (int r = -1; r < 2; r++)
+            {
+                for (int c  = -1; c < 2; c++)
+                {
+                    if (i + r < 0 || i + r > height - 1)
+                    {
+                        continue;
+                    }
+                    if (j + c < 0 || j + c > width - 1)
+                    {
+                        continue;
+                    }
+                    
+                    sumBlue += image[i + r][j + c].rgbtBlue;
+                    sumGreen += image[i + r][j + c].rgbtGreen;
+                    sumRed += image[i + r][j + c].rgbtRed;
+                    counter++;
+                    
+                }
+            }
+            
+            copy[i][j].rgbtBlue = round(sumBlue / counter);
+            copy[i][j].rgbtGreen = round(sumGreen / counter);
+            copy[i][j].rgbtRed = round(sumRed / counter);
+        }
+    }
+    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = copy[i][j].rgbtBlue;
+            image[i][j].rgbtRed = copy[i][j].rgbtRed;
+            image[i][j].rgbtGreen = copy[i][j].rgbtGreen;
+        }
+    }
+    return;
+}
